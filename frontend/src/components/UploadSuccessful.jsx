92a0cc6ad9c6569862adc5eff60b6723a3e1e3a9
Godwin Opara <godwinopara62@yahoo.com>
2023-05-24 18:52:37 +0100

    added backend dependencies(express, multer,cors,nodemon) and create a component that displays the uploaded image after successful upload

---
+++ frontend/src/components/UploadSuccessful.jsx
@@ -0,0 +1,15 @@
+const UploadSuccessful = () => {
+	return (
+		<div>
+			icon
+			<h2>Uploaded Successfully</h2>
+			<img src="" alt="" />
+			<div>
+				<input type="text" name="" id="" />
+				<button>Copy Link</button>
+			</div>
+		</div>
+	);
+};
+
+export default UploadSuccessful;
